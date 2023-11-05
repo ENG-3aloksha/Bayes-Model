b2ff0bcbddda24c06b5477774460200ff4313aa9
WeNny5 <abdelfatahwenny@yahoo.com>
2023-11-05 22:07:48 +0200

    final project with testing, validating and plotting

--- BayesModel.py
+++ BayesModel.py
@@ -1,22 +1,69 @@
 import functions
 from sklearn import datasets
+import matplotlib
+import matplotlib.pyplot as plt
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
 import numpy as np
-
+matplotlib.use('TkAgg')
 
 """
 This module to train and test bayes model classifier on iris dataset
 """
 
 iris = datasets.load_iris()
-X = np.array(iris.data)
+X = np.array(iris.data[:, :3])
 Y = np.array(iris.target)
 
 x_train, x_validate, x_test, y_train, y_validate, y_test = functions.train_validate_test_split(X, Y, 0.2, 0.4)
 
-# print("x_train : {}".format(x_train.shape))
-# print("x_validate : {}".format(x_validate.shape))
-# print("x_test : {}".format(x_test.shape))
-# print("y_train : {}".format(y_train.shape))
-# print("y_validate : {}".format(y_validate.shape))
-# print("y_test : {}".format(y_test.shape))
-print(type(x_train))
+classifier = GaussianNB()
+
+classifier.fit(x_train, y_train)
+
+y_pred_validate = classifier.predict(x_validate)
+accuracy = functions.calculate_accuracy(y_validate, y_pred_validate)
+builtin_accuracy = accuracy_score(y_validate, y_pred_validate)
+
+print("this is the accuracy of me validate: {}".format(accuracy))
+print("this is the builtin accuracy validate: {}".format(builtin_accuracy))
+
+y_pred_test = classifier.predict(x_test)
+accuracy = functions.calculate_accuracy(y_test, y_pred_test)
+builtin_accuracy = accuracy_score(y_test, y_pred_test)
+
+print("this is the accuracy of me test: {}".format(accuracy))
+print("this is the builtin accuracy test: {}".format(builtin_accuracy))
+
+# Create a mesh grid for plotting decision boundaries
+x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+z_min, z_max = X[:, 2].min() - 1, X[:, 2].max() + 1
+xx, yy, zz = np.meshgrid(np.arange(x_min, x_max, 0.1), np.arange(y_min, y_max, 0.1), np.arange(z_min, z_max, 0.1))
+Z = classifier.predict(np.c_[xx.ravel(), yy.ravel(), zz.ravel()])
+Z = Z.reshape(xx.shape)
+
+# Create a 3D scatter plot with decision boundaries
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+# Scatter plot for the data points
+ax.scatter(X[:, 0], X[:, 1], X[:, 2], c=Y, cmap=plt.cm.RdYlBu, marker='o')
+
+# Plot the decision boundary as a 3D surface
+xx, yy = np.meshgrid(np.linspace(X[:, 0].min(), X[:, 0].max(), 50),
+                     np.linspace(X[:, 1].min(), X[:, 1].max(), 50))
+zz = np.zeros(xx.shape)
+for i in range(len(xx)):
+    for j in range(len(yy)):
+        zz[i, j] = classifier.predict([[xx[i, j], yy[i, j], 0]])  # Assume petal length is 0
+
+ax.plot_surface(xx, yy, zz, cmap=plt.cm.RdYlBu, alpha=0.3)
+
+# Set labels for the axes
+ax.set_xlabel('Sepal Length')
+ax.set_ylabel('Sepal Width')
+ax.set_zlabel('Petal Length')
+ax.set_title('Naive Bayes Decision Boundaries for Iris Dataset (3D Features)')
+
+plt.show()
--- functions.py
+++ functions.py
@@ -4,34 +4,51 @@ This module facilitate the working of machine learning models
 """
 
 
-def train_validate_test_split (data, labels, testRatio = 0.3, valRatio = 0.3):
+def calculate_accuracy(predicted_y, y):
+    """
+    a function to calculate the accuracy of a model
+    Args:
+        predicted_y(np array): the expected labels
+        y(np array): the true labels
+
+    Returns: accuracy of predicted y to the real y
+
+    """
+    true_expected = 0
+    for i in range(len(y)):
+        if y[i] == predicted_y[i]:
+            true_expected += 1
+    return true_expected / len(y)
+
+
+def train_validate_test_split(data, labels, testRatio=0.3, valRatio=0.3):
     """
     This function splits data into 3 subsets (train, validate, test) datasets
 
     Args:
         data(2d nparray): All the dataset
         labels(1d nparray): The target values
-        testRatio(float): Ratio of splitted data
-        valRatio(float): Ratio of splitted training data
+        testRatio(float): Ratio to split data
+        valRatio(float): Ratio to split training data
 
     return:
-        three data nparrays (training, validate, test)
-        three labels nparrays (training, validate, test)
+        three data np arrays (training, validate, test)
+        three labels np arrays (training, validate, test)
     """
     data_rows = data.shape[0]
-    test_rows = int(testRatio * data_rows) #50
-    training_rows = data_rows - test_rows #100
-    validate_rows = int(valRatio * training_rows) #33
-    training_rows -= validate_rows #67
+    test_rows = int(testRatio * data_rows)
+    training_rows = data_rows - test_rows
+    validate_rows = int(valRatio * training_rows)
+    training_rows -= validate_rows
     
-    test_data = np.array(data[0 : test_rows, :]) # 0 : 50
-    training_data = np.array(data[test_rows : test_rows + training_rows, :]) #50 : 117
-    validate_data = np.array(data[training_rows + test_rows : data.shape[0], :]) # 117 : 150
+    test_data = np.array(data[0: test_rows, :])
+    training_data = np.array(data[test_rows: test_rows + training_rows, :])
+    validate_data = np.array(data[training_rows + test_rows: data.shape[0], :])
     
-    test_label = np.array(labels[0 : test_rows])
-    training_label = np.array(labels[test_rows : test_rows + training_rows]) 
-    validate_label = np.array(labels[training_rows + test_rows : labels.shape[0]])
+    test_label = np.array(labels[0: test_rows])
+    training_label = np.array(labels[test_rows: test_rows + training_rows])
+    validate_label = np.array(labels[training_rows + test_rows: labels.shape[0]])
     
-    return (training_data, validate_data, test_data, training_label, validate_label, test_label)
+    return training_data, validate_data, test_data, training_label, validate_label, test_label
     
     
